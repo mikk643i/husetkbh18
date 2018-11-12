b7aff3da5c7e25725ab99dff0714c7c866095564
schaadt94 <schaadt_design@hotmail.com>
2018-11-12 21:03:04 +0100

    en masse styling af siderne og menuen

---
+++ javascript.js
@@ -0,0 +1,18 @@
+
+
+document.addEventListener("DOMContentLoaded", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector(".menubutton").addEventListener ("click", trykPaaMenubutton)
+}
+
+function trykPaaMenubutton() {
+    console.log("tryk på menubutton");
+    document.querySelector("nav").classList.toggle("hidden")
+    //skift knappen frem og tilbage mellem kryds
+    document.querySelector(".menubutton").classList.toggle("kryds")
+
+}
+
+
