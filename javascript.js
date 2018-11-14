2c83ada16638739542190f90d175bd02d09aa981
timm0206 <timm0206@stud.kea.dk>
2018-11-14 19:47:27 +0100

    Frivillig med knapper

--- javascript.js
+++ javascript.js
@@ -13,11 +13,80 @@ function trykPaaMenubutton() {
 
 }
 
-function myFunction() {
-    var x = document.getElementById("myDIV");
+function myFunction_1() {
+    var x = document.getElementById("toggle_1");
+
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
+    document.getElementById("toggle_1").style.display = "block";
+    document.getElementById("toggle_2").style.display = "none";
+    document.getElementById("toggle_3").style.display = "none";
+    document.getElementById("toggle_4").style.display = "none";
+    document.getElementById("toggle_5").style.display = "none";
+    window.scrollTo(0, document.body.scrollHeight);
+
+}
+
+function myFunction_2() {
+    var x = document.getElementById("toggle_2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    document.getElementById("toggle_2").style.display = "block";
+    document.getElementById("toggle_1").style.display = "none";
+    document.getElementById("toggle_3").style.display = "none";
+    document.getElementById("toggle_4").style.display = "none";
+    document.getElementById("toggle_5").style.display = "none";
+    window.scrollTo(0, document.body.scrollHeight);
+
+}
+
+function myFunction_3() {
+    var x = document.getElementById("toggle_3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    document.getElementById("toggle_3").style.display = "block";
+    document.getElementById("toggle_1").style.display = "none";
+    document.getElementById("toggle_2").style.display = "none";
+    document.getElementById("toggle_4").style.display = "none";
+    document.getElementById("toggle_5").style.display = "none";
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+function myFunction_4() {
+    var x = document.getElementById("toggle_4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    document.getElementById("toggle_4").style.display = "block";
+    document.getElementById("toggle_1").style.display = "none";
+    document.getElementById("toggle_2").style.display = "none";
+    document.getElementById("toggle_3").style.display = "none";
+    document.getElementById("toggle_5").style.display = "none";
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+function myFunction_5() {
+    var x = document.getElementById("toggle_5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    document.getElementById("toggle_5").style.display = "block";
+    document.getElementById("toggle_1").style.display = "none";
+    document.getElementById("toggle_2").style.display = "none";
+    document.getElementById("toggle_3").style.display = "none";
+    document.getElementById("toggle_4").style.display = "none";
+    window.scrollTo(0, document.body.scrollHeight);
 }
